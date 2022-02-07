772355c470c263560e675c313cf61c1383144b96
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-02-07 16:47:24 +0000

    Done

--- 1AlphabetList.py
+++ 1AlphabetList.py
@@ -1,2 +1,11 @@
+#displays all the elements in the list
 letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 print(letters)
+
+#Display the second letter in the alphabet
+for index  in range(1,26,2):
+    print(letters[index])
+
+    #displays the remaining letters
+    print(letters[index-1])
+
